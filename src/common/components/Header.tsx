f7b3519e0cb7950462425f1727911877231621ec
“abdymm” <abdymalikmulky@gmail.com>
2023-02-18 18:02:14 +0700

    :hammer: added label on back button

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 
 import {Colors} from '@Constants';
 import {Appbar} from 'react-native-paper';
-import {NavigationProps} from '@Navigations/Routes';
+import {NavigationProps, RouteNames} from '@Navigations/Routes';
 import {useNavigation} from '@react-navigation/native';
 import {ViewProps} from 'react-native-svg/lib/typescript/fabric/utils';
 import {Fonts} from '@Constants/Metrics';
 import {ms, ScaledSheet} from 'react-native-size-matters';
+import Text from './Text';
 
 export interface HeaderProps {
   title?: string;
@@ -16,6 +17,19 @@ export interface HeaderProps {
   arrowColor?: string;
 }
 
+const getReadableRouteName = (originalRouteName: string) => {
+  switch (originalRouteName) {
+    case RouteNames.Root:
+      return 'Search';
+
+    case RouteNames.Restaurant:
+      return 'All Cuisines';
+
+    default:
+      break;
+  }
+};
+
 const Header = ({
   title,
   hideBackButton = false,
@@ -23,6 +37,11 @@ const Header = ({
   arrowColor = Colors.gray,
 }: HeaderProps) => {
   const navigation = useNavigation<NavigationProps>();
+  const index = navigation.getState().index;
+  const prevRouteName = getReadableRouteName(
+    navigation.getState().routeNames[index - 1],
+  );
+
   const onGoBack = () => {
     navigation.goBack();
   };
@@ -35,6 +54,7 @@ const Header = ({
           color={arrowColor}
         />
       )}
+      <Text style={styles.prevRoute}>{prevRouteName}</Text>
       {!!title && <Appbar.Content title={title} titleStyle={styles.title} />}
     </Appbar.Header>
   );
@@ -48,5 +68,8 @@ const styles = ScaledSheet.create({
   title: {
     fontSize: Fonts.body,
     fontWeight: '500',
+  },
+  prevRoute: {
+    color: Colors.subTitle,
   },
 });
